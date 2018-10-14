28db9d006b5356f98c17d5957040457391a954d7
Vivian Uy <vivian_uy@dlsu.edu.ph>
2018-10-14 19:03:53 +0800

    added preacts page for "submitters"

--- controllers/preacts.js
+++ controllers/preacts.js
@@ -1,5 +1,18 @@
+let preactsService = require('./../models/preactsService')
+
 module.exports.controller = function(app) {
+    
+    //for approvers
     app.get('/preacts', function(req, res) {
         res.render('preacts')
+    });
+    
+    //preacts page for submitters
+    app.get('/preacts-submission', function(req, res) {
+//        if(req.session.uid == null){
+//            resp.redirect('/');
+//        } else {
+            res.render('preacts-submit');
+        //}
     });
 }
