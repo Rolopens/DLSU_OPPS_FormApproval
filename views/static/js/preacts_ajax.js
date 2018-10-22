1d028f53d14779af192bef7c5edb356f0934c1d8
Rolopens <rolopens@gmail.com>
2018-10-22 18:23:29 +0800

    Fixed ajax bug on viewing (BUGFIX: Quick view would only display elements of last form in the list)

--- views/static/js/preacts_ajax.js
+++ views/static/js/preacts_ajax.js
@@ -1,27 +1,53 @@
 var test = document.getElementById("formLength").value
 var length = parseInt(test, 10)
-for (var i = 0; i < length; i++) {
-    var form_id = $("#item-" + i).attr("data-id")
-    document.getElementById("item-" + i).addEventListener("click", function () {
-        //ajax request to see the details of the form in quick view
-        $.ajax({
-            url: "preacts/" + form_id,
-            method: "get",
-            contentType: 'application/json',
-            success: function (formData) {
-                var form = JSON.parse(JSON.stringify(formData));
-                $("#Title").text(form["title"]);
-                $("#Nature").text(form["nature"]);
-                $("#Type").text(form["typeOfActivity"]);
-                $("#startDate").text(form["startDate"]);
-                $("#startTime").text(form["startDate"]);
-                $("#location").text(form["venue"]);
-                $("#status").text(form["status"]);
-                $("#sidebar").attr("data-id", form["_id"]);
-            }
-        })
+//for (var i = 0; i < length; i++) {
+//    var form_id = $("#item-" + i).attr("data-id")
+//    
+//    document.getElementById("item-" + i).addEventListener("click", function () {
+//        //ajax request to see the details of the form in quick view
+//        
+//        $.ajax({
+//            url: "preacts/" + form_id,
+//            method: "get",
+//            contentType: 'application/json',
+//            success: function (formData) {
+//                var form = JSON.parse(JSON.stringify(formData));
+//                $("#Title").text(formData["title"]);
+//                $("#Nature").text(formData["nature"]);
+//                $("#Type").text(formData["typeOfActivity"]);
+//                $("#startDate").text(formData["startDate"]);
+//                $("#startTime").text(formData["startDate"]);
+//                $("#location").text(formData["venue"]);
+//                $("#status").text(formData["status"]);
+//                $("#sidebar").attr("data-id", formData["_id"]);
+//            }
+//        })
+//    })
+//}
+
+
+function quickDisplay(form_id) {
+    //ajax request to see the details of the form in quick view
+
+    $.ajax({
+        url: "preacts/" + form_id,
+        method: "get",
+        contentType: 'application/json',
+        success: function (formData) {
+            var form = JSON.parse(JSON.stringify(formData));
+            $("#Title").text(formData["title"]);
+            $("#Nature").text(formData["nature"]);
+            $("#Type").text(formData["typeOfActivity"]);
+            $("#startDate").text(formData["startDate"]);
+            $("#startTime").text(formData["startDate"]);
+            $("#location").text(formData["venue"]);
+            $("#status").text(formData["status"]);
+            $("#sidebar").attr("data-id", formData["_id"]);
+        }
     })
 }
+
+
 
 document.getElementById("ApproveForm").addEventListener("click", function () {
     var approvedID = $("#sidebar").attr("data-id")
@@ -31,8 +57,8 @@ document.getElementById("ApproveForm").addEventListener("click", function () {
         contentType: 'application/json',
         success: function (formData) {
             var form = JSON.parse(JSON.stringify(formData));
-//            console.log(form)
-            $("#status").text(form["status"]);
+            //            console.log(form)
+            $("#status").text(formData["status"]);
         }
     })
 })
@@ -45,8 +71,8 @@ document.getElementById("RejectForm").addEventListener("click", function () {
         contentType: 'application/json',
         success: function (formData) {
             var form = JSON.parse(JSON.stringify(formData));
-//            console.log(form)
-            $("#status").text(form["status"]);
+            //            console.log(form)
+            $("#status").text(formData["status"]);
         }
     })
 })
