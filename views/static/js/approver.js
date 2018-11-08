cec5525eeb46359b9ea574564dd2f9c5bbe8c9c7
Miggy <allamas0726@gmail.com>
2018-11-08 18:16:35 +0800

    Check / X icons change depending on status of activity; Reducing width of Accept/Reject buttons, slight recoloring

--- views/static/js/approver.js
+++ views/static/js/approver.js
@@ -6,22 +6,24 @@ $(document).ready(function(){
 //	$('#sidebar').css("height", (offsetHeight * 100) + "%");
 //	$('#main-content .content-feed').css("max-height", ($(window).innerHeight() - $('#main-content .content-feed').offset().top));
 
-	$('#main-content .content-wrapper').click(function(){
-		$('#main-content .content-wrapper').removeClass('active');
-		$(this).addClass('active');
-	});
+	
+});
 
-	$('#select-all').click(function(){
-		if($('#select-all input').is(':checked'))
-			$('#main-content .content-feed .content-wrapper label input').prop('checked', true);
+$(document).on('click', '#main-content .content-wrapper', function(){
+    $('#main-content .content-wrapper').removeClass('active');
+    $(this).addClass('active');
+});
 
-		else $('#main-content .content-feed .content-wrapper label input').prop('checked', false);
-	});
+$(document).on('click', '#select-all', function(){
+    if($('#select-all input').is(':checked'))
+        $('#main-content .content-feed .content-wrapper label input').prop('checked', true);
 
-	$('#acts-nav ul li').click(function(){
-		$('#acts-nav ul li').removeClass('active');
-		$(this).addClass('active');
-	});
+    else $('#main-content .content-feed .content-wrapper label input').prop('checked', false);
+});
+
+$(document).on('click', '#acts-nav ul li', function(){
+    $('#acts-nav ul li').removeClass('active');
+    $(this).addClass('active');
 });
 
 $(window).resize(function(){
