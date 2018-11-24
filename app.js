7e29b2a04c8f03953fb96335429ae89155e9cc5e
Kaye Ng <kayeval@kaye-asus.laptop>
2018-11-25 01:48:12 +0800

    added one partial, fixed create page views and autocomplete

--- app.js
+++ app.js
@@ -16,31 +16,33 @@ let appSession = session({
     secret: values.key
 })
 
-
 app.set('port', process.env.PORT || values.port)
 app.set('views', __dirname + values.views)
 app.set('view engine', 'ejs')
 
 app.use(appSession)
 app.use(parser.json())
-app.use(parser.urlencoded({ extended: false }));
-app.use(function(request, response, next) {
+app.use(parser.urlencoded({
+    extended: false
+}));
+app.use(function (request, response, next) {
     response.set('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0')
     next()
 })
 
 /* set up static view files */
 app.use(express.static(__dirname + values.static))
+app.set('views', path.join(__dirname, 'views'))
 
 /* set up controllers */
-fs.readdirSync('./controllers').forEach(function(file) {
-  if(file.substr(-3) == '.js') {
-      route = require('./controllers/' + file)
-      route.controller(app)
-  }
+fs.readdirSync('./controllers').forEach(function (file) {
+    if (file.substr(-3) == '.js') {
+        route = require('./controllers/' + file)
+        route.controller(app)
+    }
 })
 
-app.listen(app.get('port'), function(){
+app.listen(app.get('port'), function () {
     console.log('[' + (new Date()).toLocaleString() + '] ' + 'Server has started')
     console.log('[' + (new Date()).toLocaleString() + '] ' + 'Server running at http://localhost:' + values.port)
 })
